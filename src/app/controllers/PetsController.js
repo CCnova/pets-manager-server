a94f853a59f6669e3ce95547f86e9187f2732b2e
CCnova <caiocnova1@gmail.com>
2021-01-19 17:55:32 -0300

    feature(pets): Finish implementation of CRUD methods for pets

--- src/app/controllers/PetsController.js
+++ src/app/controllers/PetsController.js
@@ -1,8 +1,9 @@
 const { post } = require("../../routes/pets");
 const { Pet } = require('../../database/models');
+const { getById } = require("./UserController");
 
 const petsController = {
-  async get(req, res, next) {
+  async get(req, res) {
     try {
       const pets = await Pet.findAll();
 
@@ -12,11 +13,55 @@ const petsController = {
     }
   },
 
-  async post(req, res, next) {
+  async getById(req, res) {
+    try {
+      const pet = await Pet.findOne({
+        where: {
+          id: req.params.id
+        }
+      });
+
+      return res.status(200).send({ Pet: pet });
+    } catch(error) {
+      throw(error);
+    }
+  },
+
+  async post(req, res) {
     try {
       const newPet = await Pet.create(req.body);
 
       return res.status(200).send({ Pet: newPet });
+    } catch(error) {
+      throw(error);
+    }
+  },
+
+  async put(req, res) {
+    try {
+      const [,[updatedPet]] = await Pet.update(req.body, {
+        returning: true,
+        where: {
+          id: req.params.id,
+        }
+      });
+
+      return res.status(200).send({ Pet: updatedPet });
+    } catch(error) {
+      throw(error);
+    }
+  },
+
+  async delete(req, res) {
+    try {
+      const deletedPet = await Pet.destroy({
+        returning: true,
+        where: {
+          id: req.params.id,
+        }
+      });
+
+      return res.status(200).send({ Pet: deletedPet });
     } catch(error) {
       throw(error);
     }
--- src/routes/pets.js
+++ src/routes/pets.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const { petsController } = require('../app/controllers');
 
 router.get('/', petsController.get);
+router.get('/:id', petsController.getById);
 router.post('/', petsController.post);
+router.put('/:id', petsController.put);
+router.delete('/:id', petsController.delete);
 
 module.exports = router;
